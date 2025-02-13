26cdf137adc9a6cfcf9afd65d336e566cc0722b5
AgFelix <marcelo12543@hotmail.com>
2025-02-12 21:24:15 -0300

    ✨ feat: Criação de componente footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const GlobalStyle = styled.div`
+  font-family: ${({ theme }) => theme.fonts.primary};
+`;
+
+const FooterContainer = styled.footer`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  padding: 1rem;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 60px; // Altura do footer
+  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1); // Sombra para dar um efeito de elevação
+`;
+
+const Footer = () => (
+    <>
+    <GlobalStyle/>
+  <FooterContainer>
+    <p>Sonus © 2023. Todos os direitos reservados.</p>
+  </FooterContainer>
+  </>
+);
+
+export default Footer;
