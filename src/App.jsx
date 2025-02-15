f8ca23bf1888b3af5aa784dce173f254e12b1ff5
AgFelix <marcelo12543@hotmail.com>
2025-02-15 06:54:01 -0300

    🧪 test: Criando novo teste

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@ import Dashboard from "./Pages/Dashboard";
 import Form from "./Pages/Formulario";
 import ResponsePage from "./Components/ResponsePage";
 
-const App = () => (
-  <Router>
-    <Routes>
-    <Route path="/" element={<Dashboard />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/form" element={<Form />} />
-      <Route path="/response" element={<ResponsePage/>} />
-    </Routes>
-  </Router>
-);
+
+const App = () => {
+  const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
+
+  if (!isMobile) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "20%" }}>
+        <h2>O acesso é permitido apenas via celular 📱</h2>
+      </div>
+    );
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/response" element={<ResponsePage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
